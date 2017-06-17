856363d737b5d4fdb4f5cdefe19a4de77e2891b7
shihabkabir <joinskabir@gmail.com>
2017-06-17 11:48:54 +0600

    first work with login only

---
+++ src/firstjavawork/Firstjavaclass.java
@@ -0,0 +1,28 @@
+package firstjavawork;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.By.ByXPath;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+
+public class Firstjavaclass {
+
+	public static void main(String[] args) {
+System.setProperty("webdriver.gecko.driver", "F:\\IT Class Selenium\\Software\\Gecko Driver\\geckodriver.exe");
+WebDriver driver = new FirefoxDriver();
+
+driver.get("http://newtours.demoaut.com/");
+
+driver.findElement(By.xpath("html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[3]/form/table/tbody/tr[4]/td/table/tbody/tr[2]/td[2]/input")).sendKeys("demo");
+driver.findElement(By.xpath("html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[3]/form/table/tbody/tr[4]/td/table/tbody/tr[3]/td[2]/input")).sendKeys("demo");
+driver.findElement(By.xpath("html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[2]/td[3]/form/table/tbody/tr[4]/td/table/tbody/tr[4]/td[2]/div/input")).click();
+driver.manage().timeouts().implicitlyWait(20,TimeUnit.SECONDS);
+
+driver.findElement(By.xpath("html/body/div[1]/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[2]/td[2]/b/font/input[2]")).click();
+driver.findElement(By.xpath("html/body/div/table/tbody/tr/td[2]/table/tbody/tr[4]/td/table/tbody/tr/td[2]/table/tbody/tr[5]/td/form/table/tbody/tr[9]/td[2]/font/font/input[1]")).click();
+
+	}
+
+}
